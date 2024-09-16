4185bed8da27a6c0b00b943b02f9c5229e8a377a
Mehdi Karim <contact@mehdikarim.com>
2024-09-17 01:01:29 +0700

    Fix  AskForm component and remove unused dependencies

--- src/components/ui/AskForm/AskForm.tsx
+++ src/components/ui/AskForm/AskForm.tsx
@@ -1,16 +1,13 @@
 'use client';
 
-import { useState, useActionState } from 'react';
-import { submit } from './action';
+import { useState } from 'react';
 
 const pagination = [0, 1];
 
 const AskForm: React.FC = () => {
-  const [state, action, isPending] = useActionState(submit, undefined);
   const [currentStep, setCurrentStep] = useState(0);
 
   console.log('currentStep', currentStep);
-  console.log('state', state);
 
   const handleNextStep = (): void => {
     if (currentStep < pagination.length - 1) {
@@ -25,40 +22,39 @@ const AskForm: React.FC = () => {
   };
 
   return (
-    <form action={action} className="h-screen w-screen @container/form">
-      <div
-        className={`relative flex h-full w-[${pagination.length * 100}vw] transition-all duration-300 ease-in -translate-x-[${currentStep * 100}vw]`}
-      >
+    <div className="flex h-screen items-center justify-center overflow-hidden">
+      <div className="relative h-[80vh] w-screen">
         <div
-          className={`trasistion-all relative flex h-full w-full items-center justify-center bg-amber-500 duration-700 ease-in @container/Step1 ${currentStep === 0 ? 'opacity-100' : 'opacity-0'}`}
+          className={`relative flex h-full w-[200vw] transition-all duration-300 ease-in @container/Slider ${currentStep === 1 ? '-translate-x-[100vw]' : ''}`}
         >
-          <h2 className="text-9xl text-white">Step 1</h2>
-        </div>
+          <div
+            className={`trasistion-all relative flex h-full w-screen items-center justify-center bg-amber-500 duration-700 ease-in @container/Step1 ${currentStep === 0 ? 'opacity-100' : 'opacity-0'}`}
+          >
+            <h2 className="text-9xl text-white">Step 1</h2>
+            <button
+              className="w-20 rounded-full bg-gray-300 p-2"
+              onClick={handleNextStep}
+              disabled={currentStep === pagination.length - 1}
+            >
+              Next
+            </button>
+          </div>
 
-        <div
-          className={`trasistion-all relative flex h-full w-full items-center justify-center bg-emerald-500 duration-700 ease-in @container/Step1 ${currentStep === 1 ? 'opacity-100' : 'opacity-0'}`}
-        >
-          <h2 className="text-9xl text-white">Step 2</h2>
+          <div
+            className={`trasistion-all relative flex h-full w-screen items-center justify-center bg-emerald-500 duration-700 ease-in @container/Step2 ${currentStep === 1 ? 'opacity-100' : 'opacity-0'}`}
+          >
+            <h2 className="text-9xl text-white">Step 2</h2>
+            <button
+              className="rounded-full bg-gray-300 p-2"
+              onClick={handlePreviousStep}
+              disabled={currentStep === 0}
+            >
+              Previous
+            </button>
+          </div>
         </div>
       </div>
-
-      <div className="absolute bottom-24 flex w-full justify-between px-4">
-        <button
-          className="rounded-full bg-gray-300 p-2"
-          onClick={handlePreviousStep}
-          disabled={currentStep === 0 || isPending}
-        >
-          Previous
-        </button>
-        <button
-          className="w-20 rounded-full bg-gray-300 p-2"
-          onClick={handleNextStep}
-          disabled={currentStep === pagination.length - 1 || isPending}
-        >
-          Next
-        </button>
-      </div>
-    </form>
+    </div>
   );
 };
 
